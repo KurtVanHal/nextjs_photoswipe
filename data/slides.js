4d1893f1c178fd5e3f7e28d915aa88176ca94e99
Kurt Van Hal <kurt_van_hal@hotmail.com>
2021-05-16 06:06:31 +0200

    First Commit

---
+++ data/slides.js
@@ -0,0 +1,94 @@
+const slides = [
+  {
+    src: '/images/1.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/2.jpg',
+    w: 2016,
+    h: 1512,
+    title: 'Testing',
+  },
+  {
+    src: '/images/3.jpg',
+    w: 2016,
+    h: 1512,
+    title: 'Testing',
+  },
+  {
+    src: '/images/4.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/5.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/6.jpg',
+    w: 2016,
+    h: 1512,
+    title: 'Testing',
+  },
+  {
+    src: '/images/7.jpg',
+    w: 1504,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/8.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/9.jpg',
+    w: 1504,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/10.jpg',
+    w: 720,
+    h: 540,
+    title: 'Testing',
+  },
+  {
+    src: '/images/11.jpg',
+    w: 1504,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/12.jpg',
+    w: 2016,
+    h: 1512,
+    title: 'Testing',
+  },
+  {
+    src: '/images/13.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/14.jpg',
+    w: 1512,
+    h: 2016,
+    title: 'Testing',
+  },
+  {
+    src: '/images/15.jpg',
+    w: 2016,
+    h: 1512,
+    title: 'Testing',
+  },
+];
+
+export default slides;
